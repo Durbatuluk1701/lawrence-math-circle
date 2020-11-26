f4d02e21427f252bb45c178be8a78d75d61b5398
Will Thomas <30wthomas@gmail.com>
2020-11-26 01:10:39 -0700

    Removing Old Routing

--- website/src/Components/App/App.jsx
+++ website/src/Components/App/App.jsx
@@ -9,22 +9,10 @@ import {
 } from "../../Components"
 import { Route, Switch } from "react-router-dom"
 
-const pageValues = {
-    "home": 0,
-    "calendar": 1,
-    "about-us": 2,
-    "contact": 3
-}
-
-const hrefToKey = () => {
-    return pageValues[(window.location.href.split("/").reverse()[0])];
-}
-
-
 export const App = () => {
     return (
         <>
-            <Header navHref={hrefToKey()} />
+            <Header />
             <Switch>
                 <Route exact path="/">
                     <Home />
